e8f9c6ed09c856e604b692b3f3fc7b55062cfa00
Rafael <Bytheway0708@gmail.com>
2020-04-20 12:47:12 -0400

    Removed Devtools

--- src/App.js
+++ src/App.js
@@ -16,8 +16,8 @@ import { NewClient } from './components/Screens/Clients/New';
 import { NewPayment } from './components/Screens/Payments/New';
 import { ClientProfile } from './components/Screens/Clients/Profile';
 import 'react-bootstrap-typeahead/css/Typeahead.css';
-const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-const store = createStore(rootReducer, compose(applyMiddleware(thunk), reduxDevTools));
+
+const store = createStore(rootReducer, compose(applyMiddleware(thunk)));
 if (localStorage.getItem('user')) {
   store.dispatch({ type: 'LOGIN_SUCCEEDED', payload: JSON.parse(localStorage.getItem('user')) })
 }
