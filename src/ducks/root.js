965216e433e18ccef0839c0a66fe2165292ee917
Rafael <Bytheway0708@gmail.com>
2020-05-04 00:55:23 -0400

    Public api set

--- src/ducks/root.js
+++ src/ducks/root.js
@@ -2,8 +2,8 @@ import { combineReducers } from "redux";
 import { sessionReducer } from "./session";
 import { agentReducer, clientReducer } from "./agents";
 import { notificationReducer } from "./notifications";
-//export const API = 'https://capi.megabrokerslatam.com'
-export const API = 'http://127.0.0.1:200'
+export const API = 'https://capi.megabrokerslatam.com'
+//export const API = 'http://127.0.0.1:200'
 export const rootReducer = combineReducers(
     {
         session: sessionReducer,
