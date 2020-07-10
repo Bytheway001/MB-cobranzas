605617854a630dadb8acc8d99c497ce7beb8d5c1
Rafael <Bytheway0708@gmail.com>
2020-07-10 13:45:50 -0400

    Added policy payments card

--- src/components/Screens/Expenses/PolicyPaymentPage.jsx
+++ src/components/Screens/Expenses/PolicyPaymentPage.jsx
@@ -1,20 +1,34 @@
 import React, { useState } from 'react';
-import { Row, Card, Col } from 'react-bootstrap';
+import { Row, Card, Col, Table } from 'react-bootstrap';
 import { PaymentPolicyForm } from './components/PolicyForm';
 import ClientSelect from '../../custom/ClientSelect';
 import { PolicyView } from '../Clients/PolicyView';
+import { CustomCard } from '../../custom/CustomCard';
+import { API } from '../../../ducks/root';
+import Axios from 'axios';
 
 export const PolicyPaymentsPage = () => {
     const [client, setClient] = useState([]);
+    const[payments,setPayments]=useState([]);
+    const getClient = (client)=>{
+        console.log(client)
+        Axios.get(API+'/payments/'+client[0].id).then(res=>{
+            
+            console.log(res.data.data)
+            setPayments(res.data.data)
+            setClient(client)
+        })
+       
+    }
     return (
         <Row>
             <Col sm={4}>
                 <Card className='h-100'>
                     <Card.Header className='bg-primary text-white'>Seleccion de Cliente</Card.Header>
                     <Card.Body>
-                        <ClientSelect onChange={setClient} selected={client} />
-                        { client[0] && <PolicyView client={client[0]}/>}
-                        
+                        <ClientSelect onChange={getClient} selected={client} />
+                        {client[0] && <PolicyView client={client[0]} />}
+
                     </Card.Body>
                 </Card>
             </Col>
@@ -25,6 +39,31 @@ export const PolicyPaymentsPage = () => {
                         <PaymentPolicyForm selectedClient={client} />
                     </Card.Body>
                 </Card>
+            </Col>
+            <Col sm={4}>
+                <CustomCard title='Cobranzas realizadas'>
+                    {client[0] && (
+                        <Table>
+                            <thead>
+                            <tr>
+                                <th>Fecha</th>
+                                <th>Monto</th>
+                                <th>Moneda</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                                {payments.map(p=>(
+                                    <tr>
+                                        <td>{p.payment_date}</td>
+                                        <td>{p.amount}</td>
+                                        <td>{p.currency}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                            
+                        </Table>
+                    )}
+                </CustomCard>
             </Col>
         </Row>
     )
