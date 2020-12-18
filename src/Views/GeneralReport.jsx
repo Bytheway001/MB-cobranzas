b9a01156af29bdf82a2838bf531cfe614be11af8
Rafael <Bytheway0708@gmail.com>
2020-12-18 17:38:57 -0400

    Put the tables inside SmartCards

--- src/Views/GeneralReport.jsx
+++ src/Views/GeneralReport.jsx
@@ -3,6 +3,7 @@ import React, { useEffect } from "react";
 import { useState } from "react";
 
 import { Row, Col, Table } from "react-bootstrap";
+import { SmartCard } from "../components/library/SmartCard";
 import { API, formatMoney } from "../utils/utils";
 
 const GeneralReport = () => {
@@ -78,9 +79,6 @@ const GeneralReport = () => {
 	var cobranzas = getGyPCobranzas();
 	var policyPayments = getGyPPolicyPayments();
 	var gastos = getGyPExpenses();
-
-	console.log(gastos);
-
 	ingresos.Totales.BOB += cobranzas.BOB;
 	ingresos.Totales.USD += cobranzas.USD;
 	gastos.Totales.USD += policyPayments;
@@ -91,55 +89,57 @@ const GeneralReport = () => {
 				<h1 className="text-center">Estado de GyP</h1>
 			</Col>
 			<Col sm={6}>
-				<h3>Ingresos</h3>
-				<Table size="sm" variant="bordered">
-					<thead>
-						<tr>
-							<th style={{ width: "50%" }}>--</th>
-							<th className="text-right">USD</th>
-							<th className="text-right">BOB</th>
-						</tr>
-						<tr>
-							<th>Cobranzas Realizadas</th>
-							<td className="text-right">{formatMoney(cobranzas.USD, 2, ".", ",", "$ ")}</td>
-							<td className="text-right">{formatMoney(cobranzas.BOB, 2, ".", ",", "Bs. ")}</td>
-						</tr>
-
-						{Object.keys(ingresos).map((obj, key) => (
-							<tr key={key}>
-								<th>{obj}</th>
-								<td className="text-right">{formatMoney(ingresos[obj].USD, 2, ".", ".", "$ ")}</td>
-								<td className="text-right">{formatMoney(ingresos[obj].BOB, 2, ".", ".", "Bs. ")}</td>
+				<SmartCard title="Ingresos">
+					<Table size="sm" variant="bordered">
+						<thead>
+							<tr>
+								<th style={{ width: "50%" }}>--</th>
+								<th className="text-right">USD</th>
+								<th className="text-right">BOB</th>
 							</tr>
-						))}
-					</thead>
-				</Table>
+							<tr>
+								<th>Cobranzas Realizadas</th>
+								<td className="text-right">{formatMoney(cobranzas.USD, 2, ".", ",", "$ ")}</td>
+								<td className="text-right">{formatMoney(cobranzas.BOB, 2, ".", ",", "Bs. ")}</td>
+							</tr>
+
+							{Object.keys(ingresos).map((obj, key) => (
+								<tr key={key}>
+									<th>{obj}</th>
+									<td className="text-right">{formatMoney(ingresos[obj].USD, 2, ".", ".", "$ ")}</td>
+									<td className="text-right">{formatMoney(ingresos[obj].BOB, 2, ".", ".", "Bs. ")}</td>
+								</tr>
+							))}
+						</thead>
+					</Table>
+				</SmartCard>
 			</Col>
 			<Col sm={6}>
-				<h3>Egresos</h3>
-				<Table size="sm" variant="bordered">
-					<thead>
-						<tr>
-							<th style={{ width: "50%" }}>--</th>
-							<th className="text-right">USD</th>
-							<th className="text-right">BOB</th>
-						</tr>
-						<tr>
-							<th>Pago de Polizas</th>
-							<td className="text-right">{formatMoney(policyPayments, 2, ".", ",", "$ ")}</td>
-							<td className="text-right">{0}</td>
-						</tr>
-
-						{Object.keys(gastos).map((obj, key) => (
-							<tr key={key}>
-								<th>{obj}</th>
-
-								<td className="text-right">{formatMoney(gastos[obj].USD, 2, ".", ".", "$ ")}</td>
-								<td className="text-right">{formatMoney(gastos[obj].BOB, 2, ".", ".", "Bs. ")}</td>
+				<SmartCard title="Egresos">
+					<Table size="sm" variant="bordered">
+						<thead>
+							<tr>
+								<th style={{ width: "50%" }}>--</th>
+								<th className="text-right">USD</th>
+								<th className="text-right">BOB</th>
 							</tr>
-						))}
-					</thead>
-				</Table>
+							<tr>
+								<th>Pago de Polizas</th>
+								<td className="text-right">{formatMoney(policyPayments, 2, ".", ",", "$ ")}</td>
+								<td className="text-right">{0}</td>
+							</tr>
+
+							{Object.keys(gastos).map((obj, key) => (
+								<tr key={key}>
+									<th>{obj}</th>
+
+									<td className="text-right">{formatMoney(gastos[obj].USD, 2, ".", ".", "$ ")}</td>
+									<td className="text-right">{formatMoney(gastos[obj].BOB, 2, ".", ".", "Bs. ")}</td>
+								</tr>
+							))}
+						</thead>
+					</Table>
+				</SmartCard>
 			</Col>
 		</Row>
 	);
